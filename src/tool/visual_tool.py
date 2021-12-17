16f8df5f7b363537a02726ddcb14f8ba04a9983c
luxunxiansheng <si.yuan@yahoo.com>
2021-12-17 22:12:41 +0800

    increase the font size

--- src/tool/visual_tool.py
+++ src/tool/visual_tool.py
@@ -4,7 +4,7 @@ from torchvision.utils import draw_bounding_boxes
 
 def draw_img_bboxes_labels(image, bboxes, labels,resize_shape=[800,800],colors='red'):
     bboxes = torch.index_select(bboxes.cpu(),1,torch.tensor([1,0,3,2]))
-    img_and_bbox = draw_bounding_boxes(image.cpu(),bboxes.cpu(),labels,width=2,colors=colors,font_size=20)
+    img_and_bbox = draw_bounding_boxes(image.cpu(),bboxes.cpu(),labels,width=2,colors=colors,font_size=40)
     transform = transforms.Compose([transforms.Resize(resize_shape)])
     img_and_bbox = transform(img_and_bbox)
     return img_and_bbox
